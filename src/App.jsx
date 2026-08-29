96f72ce3375aa398e3560fa7ab35582ca421bf32
Rajkumar J <rajkumar.work.in@gmail.com>
2026-08-29 10:29:42 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Sri Hari Electricals
+      </h1>
+    </div>
+  )
+}
+
+export default App
