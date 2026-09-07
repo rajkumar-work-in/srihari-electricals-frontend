963ab18383f7f7dc537493991e620eaf9af943b1
Rajkumar J <rajkumar.work.in@gmail.com>
2026-09-07 07:25:42 -0700

    updated hero section

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -25,7 +25,7 @@ function HeroSlider({ images, interval = 4500 }) {
 
       <div className="absolute inset-0 bg-black/75" />
 
-      {/* Dot  */}
+      {/* Dot indicators */}
       <div className="absolute top-6 right-6 flex gap-2 z-10">
         {images.map((_, i) => (
           <button
